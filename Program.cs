1bf72c3d1675a6117970ebc5fff0c256281dbe07
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-08-19 09:35:30 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<string> names = new List<string>()
+{
+    "Jordan Williams", "Travis Williams", "Dustin Dixon"
+};
+
+Console.WriteLine("");
+Console.WriteLine("*** First Iteration ***");
+Console.WriteLine("------------------------");
+
+foreach (string name in names)
+{
+    Console.WriteLine($"Hello, {name}");
+}
+
+names.Add("Brittany Mangum");
+names.Add("David Stewart");
+names.Add("Sethward Theodosia Landon Elric Sebastian III");
+
+Console.WriteLine("");
+Console.WriteLine("*** Second Iteration ***");
+Console.WriteLine("--------------------------");
+
+foreach (string name in names)
+{
+    if (name.Length > 20) {
+        Console.WriteLine($"Hello, {name}");
+    }
+}
